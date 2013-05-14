fe47ac5adc331e2f9b2d29cd617a94d0a48cae29
lax <xingkungao@gmail.com>
2013-05-14 18:38:15 +0800

    implement printk

--- src/lib/vfprintf.c
+++ src/lib/vfprintf.c
@@ -11,10 +11,39 @@
  */
 int
 vfprintf(void (*putchar_func)(char), const char *format, void **data) {
-	const char *s;
-	for (s = "please implement me\n"; *s; s ++) {
-		putchar_func(*s);
+	char ch;
+	char *str;
+	char *pbuf;
+	while(1){
+		while((ch=*format++)!='%'){
+			if(ch=='\0')
+			      return 0;
+			putchar_func(ch);
+		}
+		switch ((ch=*format++)){
+			case 'c':
+				putchar_func(*(char *)data++);
+				break;
+			case 'd':
+				pbuf=itoa(*(int *)data++,10);
+				while((ch=*pbuf++)!=0)
+				      putchar_func(ch);
+				break;
+			case 'x':
+				pbuf=utoa(*(unsigned int *)data++,16);
+				while((ch=*pbuf++)!=0)
+				      putchar_func(ch);
+				break;
+			case 's':
+				str=*(char **)data++;
+				while((ch=*str++)!=0)
+				      putchar_func(ch);
+				break;
+			default :
+				for (str="unsported printing format!\n"; *str; str ++)
+				      putchar_func(*str);
+				break;
+		}
 	}
 	return 0;
 }
-
